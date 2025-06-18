a89737d89624144f13f0eaa2443cb8f3c2744ecd
Johnny <Johnny@JOHNNY-LAPTOP>
2025-06-18 16:27:34 -0700

    added default admin account

--- UserManagement/Program.cs
+++ UserManagement/Program.cs
@@ -23,6 +23,15 @@ namespace UserManagement
             DataContext context = new DataContext();
             UnitOfWork unitOfWork = new UnitOfWork(context);
             UserService userService = new UserService(unitOfWork);
+
+            // ensure default admin account is created
+            var adminUser = context.Users.FirstOrDefault(u => u.UserName == "Admin");
+            if (adminUser == null)
+            {
+                unitOfWork.Users.CreateUser("admin", "password", "Admin");
+                unitOfWork.Save();
+            }
+
             context.Users.FirstOrDefault(); // warmup entity framework
             loginForm login = new loginForm(userService);
             login.StartPosition = FormStartPosition.CenterScreen;
